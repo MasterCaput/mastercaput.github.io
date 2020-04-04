44febe8be6af9139869a20f45a373674580be26a
MasterCaput <wasilisitnikov000@yandex.ru>
2020-04-05 01:09:13 +0300

    Create JS_wheel.js

---
+++ JS_wheel.js
@@ -0,0 +1,13 @@
+function wheel() {
+    let start = Date.now();
+
+    let timer = setInterval(function() {
+        let timePassed = Date.now() - start;
+
+        wl.style.right = timePassed / 21 + 'px';
+
+        if (timePassed > 9000) clearInterval(timer);
+
+    }, 0.1);
+}
+window.onload = wheel();
