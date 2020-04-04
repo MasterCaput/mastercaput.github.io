9478704cadd37abdec74d4bd8c8c1d625efbfc10
MasterCaput <wasilisitnikov000@yandex.ru>
2020-04-05 01:09:15 +0300

    Create JS_disabled.js

---
+++ JS_disabled.js
@@ -0,0 +1,13 @@
+ function disabled() {
+     let start = Date.now();
+
+     let timer = setInterval(function() {
+         let timePassed = Date.now() - start;
+
+         dis.style.right = timePassed / 7 + 'px';
+
+         if (timePassed > 3000) clearInterval(timer);
+
+     }, 0.1);
+ }
+ window.onload = disabled();
